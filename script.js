18886fc85a2b2bf330a50e1fff943a922d09cb05
rugieri <rugieri.ronaldo@gmail.com>
2021-11-11 01:39:20 -0300

    added the rest of logic of the game and fixed bug on the lights that never goes out

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let order = [];
-let clickOrder = [];
+let clickedOrder = [];
 let score = 0;
 
-/* 0- green
-   1- red
-   2- yellow
-   3- blue 
-
-*/
+//0 - green
+//1 - red
+//2 - yellow
+//3 - blue
 
 const blue = document.querySelector('.blue');
 const red = document.querySelector('.red');
@@ -16,44 +14,85 @@ const yellow = document.querySelector('.yellow');
 
 let shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4);
-  order(order.length) = colorOrder;
+  order[order.length] = colorOrder;
   clickedOrder = [];
 
-  for(let i in order) {
-    let elementColor = createColorElement(order[i])
-    lightColor(elementColor, Number(i) +1)
+  for (let i in order) {
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
   }
+};
 
-  let lightColor = (element, number) => {
-    number = number * 500;
-    setTimeout(()=> {
-      element.classList.add('selected')
-    }, number - 250)
-    setTimeout(()=> {
-      element.classList.remove('selected')
-    })
-  }
+let lightColor = (element, number) => {
+  number = number * 500;
+  setTimeout(() => {
+    element.classList.add('selected');
+  }, number - 250);
+  setTimeout(() => {
+    element.classList.remove('selected');
+  }, number);
+};
 
-  let checkOrder = () => {
-    for(let in clickOrder) {
-      if(clickOrder[i] != order[i]) {
-        lose();
-        break;
-      }
-    }
-    if(clickedOrder.length == order.length) {
-      alert(`Score ${score}\nYou Win!!! Get Ready for the Next Level !!!`)
-      nextLevel();
+let checkOrder = () => {
+  for (let i in clickedOrder) {
+    if (clickedOrder[i] != order[i]) {
+      gameOver();
+      break;
     }
   }
-  let click = (color) => {
-    clickedOrder[clickedOrder.length] = color;
-    createColorElement(color).classList.add('selected');
+  if (clickedOrder.length == order.length) {
+    alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+    nextLevel();
+  }
+};
 
-    setTimeout(() => {
-      createColorElement(color).classList.remove('selected');
-    })
+let click = (color) => {
+  clickedOrder[clickedOrder.length] = color;
+  createColorElement(color).classList.add('selected');
 
+  setTimeout(() => {
+    createColorElement(color).classList.remove('selected');
     checkOrder();
+  }, 250);
+};
+
+let createColorElement = (color) => {
+  if (color == 0) {
+    return green;
+  } else if (color == 1) {
+    return red;
+  } else if (color == 2) {
+    return yellow;
+  } else if (color == 3) {
+    return blue;
   }
-}
+};
+
+let nextLevel = () => {
+  score++;
+  shuffleOrder();
+};
+
+let gameOver = () => {
+  alert(
+    `Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`
+  );
+  order = [];
+  clickedOrder = [];
+
+  playGame();
+};
+
+let playGame = () => {
+  alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
+  score = 0;
+
+  nextLevel();
+};
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+playGame();
